55af0f74a61cfa9ce9ad140acf2d6051b4e2c9d2
Hojun Kim <hkim372@my.centennialcollege.ca>
2025-11-25 14:09:15 -0500

    Add NavBar, update Landing page, add routes for resumes/profile

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,7 @@
+export function ProfilePage() {
+  return (
+    <div className="p-10 text-xl">
+      Profile Page
+    </div>
+  );
+}
---
+++ client/src/pages/ResumesPage.jsx
@@ -0,0 +1,7 @@
+export function ResumesPage() {
+  return (
+    <div className="p-10 text-xl">
+      Resumes Page
+    </div>
+  );
+}
